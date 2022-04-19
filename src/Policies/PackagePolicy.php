01f8a175dc73d8b0c0f5204a2098c28c91b69056
Auriga Aristo <aurigaaristo@gmail.com>
2022-04-20 03:22:08 +0700

    :lock: add system policies

---
+++ src/Policies/PackagePolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+class PackagePolicy
+{
+    private string $package;
+
+    public function __construct()
+    {
+        $this->package = config('dipantry.rajaongkir.package');
+    }
+
+    public function allowGetSupportedCouriers(): bool
+    {
+        return $this->package != null;
+    }
+
+    public function allowGetProvinces(): bool
+    {
+        return $this->package != null;
+    }
+
+    public function allowGetCities(): bool
+    {
+        return $this->package != null;
+    }
+
+    public function allowGetDistricts(): bool
+    {
+        return $this->package == 'pro';
+    }
+
+    public function allowGetCosts(string $courierCode): bool
+    {
+        if ($this->package == 'starter'){
+            return $courierCode == 'jne' || $courierCode == 'pos' || $courierCode == 'tiki';
+        }
+        return $this->package == 'basic' || $this->package == 'pro';
+    }
+
+    public function allowGetInternationalOrigin(): bool
+    {
+        return $this->package == 'basic' || $this->package == 'pro';
+    }
+
+    public function allowGetInternationalDestination(): bool
+    {
+        return $this->package == 'basic' || $this->package == 'pro';
+    }
+
+    public function allowGetInternationalCosts(): bool
+    {
+        return $this->package == 'basic' || $this->package == 'pro';
+    }
+
+    public function allowGetCurrencies(): bool
+    {
+        return $this->package == 'basic' || $this->package == 'pro';
+    }
+
+    public function allowGetWaybill(string $courierCode): bool
+    {
+        if ($this->package == 'basic'){
+            return $courierCode == 'jne';
+        }
+        return $this->package == 'pro';
+    }
+}
